d9908277a595f7138d9495a6c06565a591452e04
Anders Bengtsson <anders.bengtsson@conrab.se>
2019-06-20 10:47:39 +0200

    Updates

---
+++ precache-manifest.68bfa168fdc705459e9027ca0f65becd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab51412d2cf018bebd1d63aa9a832b5a",
+    "url": "/soccer-training/index.html"
+  },
+  {
+    "revision": "e35faed8a8fac1be22a2",
+    "url": "/soccer-training/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/soccer-training/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e35faed8a8fac1be22a2",
+    "url": "/soccer-training/static/js/main.8d1f7df7.chunk.js"
+  },
+  {
+    "revision": "8abad10538ce37de2f63",
+    "url": "/soccer-training/static/js/runtime~main.3752efe7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/soccer-training/static/media/logo.5d5d9eef.svg"
+  }
+]);
