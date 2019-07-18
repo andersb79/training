7f23cac5e653fb75c97abcf2b40443dc530bf896
Anders Bengtsson <andersb79@hotmail.com>
2019-07-18 19:37:23 +0200

    progress

--- src/components/Game.js
+++ src/components/Game.js
@@ -64,7 +64,7 @@ export default function Game({ store }) {
     var file = e.target.files[0];
 
     store.processFile(file, level, text => {
-      
+      alert('Uppladdning klar')
     });
   }
 
